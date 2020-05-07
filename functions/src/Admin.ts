b69ecfec7404a41c7f817bdece1e6d85a10460d1
vhumeniuk <vladimir.humeniuk@williamhill.com>
2020-05-07 23:19:33 +0200

    added function for user custom claims

--- functions/src/Admin.ts
+++ functions/src/Admin.ts
@@ -1,8 +1,16 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+admin.initializeApp()
+
 export const getUserByEmail = functions.https.onCall((data, context) => {
   const email = data.email
 
   return admin.auth().getUserByEmail(email)
+})
+
+export const setUserRoles = functions.https.onCall((data, context) => {
+  const { uid, roles } = data
+
+  return admin.auth().setCustomUserClaims(uid, roles)
 })
